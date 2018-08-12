9962f881a0ecb2ae30429ae01fd7ddcb72cdd23d
Alexandre Melotti <lex.melotti@gmail.com>
2018-08-12 16:51:00 -0300

    Background tone and Player Audio Effects;

---
+++ Assets/Scripts/Audio/BackgroundTone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BackgroundTone : PausableBehaviour
+{
+	[SerializeField]
+	private SimpleAudioEvent backgroundTone;
+
+	[SerializeField]
+	private AudioSource audioSource;
+
+	public override void Start()
+	{
+		base.Start();
+		StartCoroutine(PlayTone(2f));
+	}
+
+	private IEnumerator PlayTone(float time)
+	{
+		backgroundTone.Play(audioSource);
+		yield return new WaitForSeconds(time);
+		time -= 0.01f;
+		if (time < 1f) time = 1f;
+
+		StartCoroutine(PlayTone(time));
+	}
+}
---
+++ Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+	[SerializeField]
+	private AudioSource audioSource;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3(horizontal, 0, vertical);
+		if(direction.magnitude > 1)
+		{
+			direction = direction.normalized;
+		}
+
+		if (direction.magnitude > 0)
+		{
+			if(!audioSource.isPlaying)
+			{
+				audioSource.Play();
+			}
+
+			float modifier = 0.04f;
+
+			if(Physics.Raycast(transform.position, -transform.up, 1,  1 << LayerMask.NameToLayer("Effects")))
+			{
+				modifier = 0.02f;
+			}
+
+			audioSource.pitch = direction.magnitude * modifier;
+		}
+		else
+		{
+			audioSource.Stop();
+		}
+	}
+}

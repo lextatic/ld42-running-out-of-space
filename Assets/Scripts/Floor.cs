d61624b4a1335560640cd9ad1398df2249e71d20
Alexandre Melotti <lex.melotti@gmail.com>
2018-08-12 02:05:42 -0300

    Player prefab with avatar and animation;

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -28,7 +28,7 @@ public class Floor : MonoBehaviour
 	{
 		yield return new WaitForSeconds(Random.Range(0f, 2f));
 		floorFallingTile.isKinematic = false;
-		yield return new WaitForSeconds(Random.Range(0f, 0.1f));
+		yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
 		floorTiltCollider.gameObject.SetActive(false);
 		yield return new WaitForSeconds(2f);
 		Destroy(gameObject);
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerAnimator : PausableBehaviour
+{
+	[SerializeField]
+	private Transform handler;
+
+	[SerializeField]
+	private Transform[] wheels;
+
+	private Vector3 lastPosition;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+		foreach (var wheel in wheels)
+		{
+			wheel.Rotate(-720 * direction.magnitude * Time.deltaTime, 0, 0);
+		}
+
+
+		var lastFrameMoveDirection = transform.position - lastPosition;
+		float handleValue = Vector3.Dot(Vector3.Cross(direction.normalized, lastFrameMoveDirection.normalized), Vector3.up);
+
+		handler.localRotation = Quaternion.Euler(25f, Mathf.Max(Mathf.Min(-360f * handleValue, 30f), -30), 0);
+
+		lastPosition = transform.position;
+	}
+}

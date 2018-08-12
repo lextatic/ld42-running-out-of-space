e337515c2098cd970a4c46cd6138ae41182e72a0
Danilo Nishimura <danilonishi@gmail.com>
2018-08-12 01:51:07 -0300

    Added small vfx to fallable tiles right after object gets collected

---
+++ Assets/CollectFloorVFX.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CollectFloorVFX : MonoBehaviour
+{
+
+	[SerializeField]
+	Transform floorsParent;
+
+	List<Floor> floors;
+
+	[SerializeField]
+	Collectible collectible;
+
+	private void Start()
+	{
+		collectible.OnObjectCollected += HandleObjectCollected;
+
+		floors = new List<Floor>(floorsParent.GetComponentsInChildren<Floor>());
+		floors.OrderBy(item => Vector3.Distance(item.transform.position, this.transform.position));
+	}
+
+	private void OnDestroy()
+	{
+		collectible.OnObjectCollected -= HandleObjectCollected;
+	}
+
+	private void HandleObjectCollected(Collectible obj)
+	{
+		Debug.Log("Collected");
+		foreach (var floor in floors)
+		{
+			floor.gameObject.AddComponent<FloorBlinkVFX>().SetDelay(Vector3.Distance(this.transform.position, floor.transform.position) * 0.06125f);
+		}
+	}
+}

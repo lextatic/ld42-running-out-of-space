72dc2360550526c0c8a263f33f6c8ffbdd368177
Danilo Nishimura <danilonishi@gmail.com>
2018-08-12 17:47:40 -0300

    Added ObjectCollected particles

---
+++ Assets/CollectParticleVFX.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectParticleVFX : MonoBehaviour
+{
+	[SerializeField]
+	ParticleSystem Particle;
+
+	[SerializeField]
+	Collectible collectible;
+
+	private void Start()
+	{
+		collectible.OnObjectCollected += HandleObjectCollected;
+	}
+
+	private void OnDestroy()
+	{
+		collectible.OnObjectCollected -= HandleObjectCollected;
+	}
+
+	private void HandleObjectCollected(Collectible obj)
+	{
+		Instantiate(Particle, this.transform.position, Quaternion.identity);
+	}
+}

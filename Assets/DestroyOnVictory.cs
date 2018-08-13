8fba1b3f8808faf4f7c188ef0d3105fd2e850862
Danilo Nishimura <danilonishi@gmail.com>
2018-08-12 23:18:54 -0300

    Added end game VFX ( WIP )

---
+++ Assets/DestroyOnVictory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnVictory : MonoBehaviour
+{
+	[SerializeField]
+	GameObject explosionPrefab;
+
+	VictoryCondition victory;
+
+	private void OnEnable()
+	{
+		victory = FindObjectOfType<VictoryCondition>();
+		victory.OnVictory += HandleGameFinished;
+	}
+
+	private void OnDisable()
+	{
+		victory.OnVictory -= HandleGameFinished;
+	}
+
+	private void HandleGameFinished(bool obj)
+	{
+		if (obj)
+		{
+			Instantiate(explosionPrefab, this.transform.position, Quaternion.identity);
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}

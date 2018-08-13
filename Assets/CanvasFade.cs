8898243d988c37fc6b17369f93622c7ab465f6be
Danilo Nishimura <danilonishi@gmail.com>
2018-08-13 02:13:10 -0300

    Added UI Fading script

---
+++ Assets/CanvasFade.cs
@@ -0,0 +1,33 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasFade : MonoBehaviour
+{
+	[SerializeField]
+	CanvasGroup canvas;
+
+	[SerializeField]
+	float targetAlpha;
+
+	[SerializeField]
+	float delay;
+
+	[SerializeField]
+	float time;
+
+	void OnEnable()
+	{
+		StartCoroutine(DoTween());
+	}
+
+
+
+	// Use this for initialization
+	IEnumerator DoTween()
+	{
+		yield return new WaitForSeconds(delay);
+		canvas.DOFade(targetAlpha, time);
+	}
+}

bf21b0db6b1d4814c15043c1b5665d510704b539
Alexandre Melotti <lex.melotti@gmail.com>
2018-08-20 00:18:08 -0300

    Fixing wall bug and updating models and effects

---
+++ Assets/Scripts/Floaty.cs
@@ -0,0 +1,10 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Floaty : MonoBehaviour
+{
+	void Start ()
+	{
+		transform.DOMoveY(0.2f, .5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
+	}
+}
